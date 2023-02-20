c1e9f520766143bbb493df262a654d8d67ba8d82
Duy <caoduy73@gmail.com>
2023-02-21 00:14:03 +0800

    Make the cursor able to draw on canvas

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-// Create a webpage with a default 16x16 grid of square divs as a canvas to sketch on
+// List of constant values
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = '#000000';
+
+// List of variables
 const canvas = document.querySelector('.canvas');
 
-// Create a function that creates a grid of divs
+// Create a grid of squares on the canvas
 function createGrid(size) {
     canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -21,4 +25,17 @@ sizeSlider.addEventListener('mouseup', () => {
     createGrid(size);
 });
 
-createGrid(16);
+// Change the color of the squares while the mouse is pressed, stop when released
+canvas.addEventListener('mousedown', (e) => {
+    if (e.target.classList.contains('square')) {
+        e.target.style.backgroundColor = DEFAULT_COLOR;
+    }
+});
+canvas.addEventListener('mousemove', (e) => {
+    console.log(e);
+    if (e.buttons === 1 && e.target.classList.contains('square')) {
+        e.target.style.backgroundColor = DEFAULT_COLOR;
+    }
+});
+
+createGrid(DEFAULT_SIZE);
